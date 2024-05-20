249134f3460677ae698959b0c63f820423d276d0
ShadowZ03 <dmitrigatto@gmail.com>
2024-05-20 06:30:10 -0400

    added docs

--- main.py
+++ main.py
@@ -3,13 +3,18 @@ import yfinance as yf
 
 # Add user input for selecting multiple tickers
 # Add user input for deciding call/put display
-# Add user input for days out
+# Add user input for weeks out
 
 ticker = yf.Ticker("ARM")
 current_price = ticker.info['currentPrice']
 
 
 def option_brief(option):
+    """
+    Grabs relevant information for options
+    :param option: "call" or "put"
+    :return: filtered dataframe copy from main dataframe
+    """
     filtered_indices = option.index[option['strike'] == int(current_price)].tolist()
 
     surrounding_indices = []
@@ -20,16 +25,22 @@ def option_brief(option):
 
     surrounding_indices = sorted(set(surrounding_indices))
     filtered_df = option.iloc[surrounding_indices]
-    return filtered_df
+    return filtered_df.copy()
 
 
-def option_seeker(days: int, option: str):
+def option_seeker(weeks: int, option: str):
+    f"""
+    Looks at current price of strike price options +/- 3 from current price, for {weeks}, on desired {option}
+    :param weeks: number of weeks seeking data
+    :param option: "call" or "put"
+    :return: appended dataframe of option, by weeks, in range of +/- 3 of current price
+    """
     master_df = None
     op = None
 
     option_filter = ['strike', 'bid', 'lastPrice', 'ask', 'contractSymbol']
 
-    for date in range(days):
+    for date in range(weeks):
         select_date = ticker.options[date]
         counter = date + 1  # Start counter from 1
 
@@ -40,26 +51,25 @@ def option_seeker(days: int, option: str):
 
         filter_call = op[option_filter]
 
-        x = option_brief(filter_call).copy()
+        x = option_brief(filter_call)
 
         value_list = []
-        value_per_days = []
+        value_per_weeks = []
 
         for i in x['bid']:
             value = (i * 100) / (counter * 5)
-            value_per_days.append(round(value))
+            value_per_weeks.append(round(value))
 
             value2 = (i * 100)
             value_list.append(round(value2))
 
         x.loc[:, 'Value'] = value_list
-        x.loc[:, 'ValuePerDays'] = value_per_days
+        x.loc[:, 'ValuePerweeks'] = value_per_weeks
 
         # Concatenate the filtered and computed DataFrame to the master DataFrame
         if not x.empty:
             master_df = pd.concat([master_df, x], ignore_index=True)
 
     return master_df
-
 
 print(option_seeker(3, "call"))

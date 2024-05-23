5482365c552dcf266c244f7f8a48e851a1f83e45
ShadowZ03 <dmitrigatto@gmail.com>
2024-05-22 21:37:25 -0400

    changed variable names for clarity, added flexibilioty for bulk stocks

--- main.py
+++ main.py
@@ -5,17 +5,13 @@ import yfinance as yf
 # Add user input for deciding call/put display
 # Add user input for weeks out
 
-ticker = yf.Ticker("ARM")
-current_price = ticker.info['currentPrice']
-
-
-def option_brief(option):
+def option_brief(option, cp: int):
     """
     Grabs relevant information for options
     :param option: "call" or "put"
     :return: filtered dataframe copy from main dataframe
     """
-    filtered_indices = option.index[option['strike'] == int(current_price)].tolist()
+    filtered_indices = option.index[option['strike'] == cp].tolist()
 
     surrounding_indices = []
     for idx in filtered_indices:
@@ -28,13 +24,17 @@ def option_brief(option):
     return filtered_df.copy()
 
 
-def option_seeker(weeks: int, option: str):
+def option_seeker(tick: str, weeks: int, option: str):
     f"""
     Looks at current price of strike price options +/- 3 from current price, for {weeks}, on desired {option}
     :param weeks: number of weeks seeking data
     :param option: "call" or "put"
     :return: appended dataframe of option, by weeks, in range of +/- 3 of current price
     """
+    ticker = yf.Ticker(tick)
+    current_price = int(ticker.info['currentPrice'])
+
+
     master_df = None
     op = None
 
@@ -51,25 +51,30 @@ def option_seeker(weeks: int, option: str):
 
         filter_call = op[option_filter]
 
-        x = option_brief(filter_call)
+        filtered_options_df = option_brief(filter_call, current_price)
 
         value_list = []
         value_per_weeks = []
 
-        for i in x['lastPrice']:
+        for i in filtered_options_df['lastPrice']:
             value = (i * 100) / (counter * 5)
             value_per_weeks.append(round(value))
 
             value2 = (i * 100)
             value_list.append(round(value2))
 
-        x.loc[:, 'Value'] = value_list
-        x.loc[:, 'ValuePerweeks'] = value_per_weeks
+        filtered_options_df.loc[:, 'Value'] = value_list
+        filtered_options_df.loc[:, 'ValuePerweeks'] = value_per_weeks
 
         # Concatenate the filtered and computed DataFrame to the master DataFrame
-        if not x.empty:
-            master_df = pd.concat([master_df, x], ignore_index=True)
+        if not filtered_options_df.empty:
+            master_df = pd.concat([master_df, filtered_options_df], ignore_index=True)
 
     return master_df
 
-print(option_seeker(3, "put"))
+stocks = ["ARM", "F"]
+
+for stock in stocks:
+    print(f"Looking at {stock}")
+    print(option_seeker(stock, 3, "put"))
+    print("*"*10)
